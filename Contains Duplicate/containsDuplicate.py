0b84008a54b222cbccb60e12ed242aed8ca041df
Rutarj Shah <128926379+rutarj@users.noreply.github.com>
2024-03-19 21:12:37 -0400

    Update containsDuplicate.py

--- Contains Duplicate/containsDuplicate.py
+++ Contains Duplicate/containsDuplicate.py
@@ -4,16 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        # Create a set to store unique elements
+        
         seen = set()
         
-        # Iterate through the array
+       
         for num in nums:
-            # If the element is already in the set, return True
+          
             if num in seen:
                 return True
-            # Otherwise, add it to the set
+        
             seen.add(num)
         
-        # If no duplicates were found, return False
+   
         return False
