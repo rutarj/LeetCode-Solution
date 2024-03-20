342094df29f892905908bd21c0682cb95b3a157e
Rutarj Shah <128926379+rutarj@users.noreply.github.com>
2024-03-19 21:12:07 -0400

    Update Valid Anagram.py

--- Valid Anagram/Valid Anagram.py
+++ Valid Anagram/Valid Anagram.py
@@ -5,21 +5,20 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        # Check if lengths are equal
+      
         if len(s) != len(t):
             return False
         
-        # Create dictionaries to store character counts for both strings
+       
         char_count_s = {}
         char_count_t = {}
         
-        # Populate char_count_s dictionary with character counts from string s
+        
         for char in s:
             char_count_s[char] = char_count_s.get(char, 0) + 1
         
-        # Populate char_count_t dictionary with character counts from string t
+   
         for char in t:
             char_count_t[char] = char_count_t.get(char, 0) + 1
-        
-        # Compare character counts in both dictionaries
+       
         return char_count_s == char_count_t
